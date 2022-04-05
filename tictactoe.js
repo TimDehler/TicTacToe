e22c25af71c738a3b214fc28c10dc62155dc558e
Tim Dehler <tim_dehler@goodyear.com>
2022-04-05 15:55:37 +0200

    Shortend version of working tic tac toe

--- tictactoe.js
+++ tictactoe.js
@@ -5,107 +5,66 @@ let roundCounter = 0;
 //console.log(roundCounter);
 
 function winningConditions() {
-  if (roundCounter < 9) {
-    if (
-      my_button_1.value == 1 &&
-      my_button_2.value == 1 &&
-      my_button_3.value == 1
-    ) {
-      console.log("Spieler 1 hat gewonnen");
-    } else if (
-      my_button_4.value == 1 &&
-      my_button_5.value == 1 &&
-      my_button_6.value == 1
-    ) {
-      console.log("Spieler 1 hat gewonnen");
-    } else if (
-      my_button_7.value == 1 &&
-      my_button_8.value == 1 &&
-      my_button_9.value == 1
-    ) {
-      console.log("Spieler 1 hat gewonnen");
-    } else if (
-      my_button_1.value == 1 &&
-      my_button_5.value == 1 &&
-      my_button_9.value == 1
-    ) {
-      console.log("Spieler 1 hat gewonnen");
-    } else if (
-      my_button_3.value == 1 &&
-      my_button_5.value == 1 &&
-      my_button_7.value == 1
-    ) {
-      console.log("Spieler 1 hat gewonnen");
-    } else if (
-      my_button_1.value == 1 &&
-      my_button_4.value == 1 &&
-      my_button_7.value == 1
-    ) {
-      console.log("Spieler 1 hat gewonnen");
-    } else if (
-      my_button_2.value == 1 &&
-      my_button_5.value == 1 &&
-      my_button_8.value == 1
-    ) {
-      console.log("Spieler 1 hat gewonnen");
-    } else if (
-      my_button_3.value == 1 &&
-      my_button_6.value == 1 &&
-      my_button_9.value == 1
-    ) {
-      console.log("Spieler 1 hat gewonnen");
-    }
-    if (
-      my_button_1.value == 2 &&
-      my_button_2.value == 2 &&
-      my_button_3.value == 2
-    ) {
-      console.log("Spieler 2 hat gewonnen");
-    } else if (
-      my_button_4.value == 2 &&
-      my_button_5.value == 2 &&
-      my_button_6.value == 2
-    ) {
-      console.log("Spieler 2 hat gewonnen");
-    } else if (
-      my_button_7.value == 2 &&
-      my_button_8.value == 2 &&
-      my_button_9.value == 2
-    ) {
-      console.log("Spieler 2 hat gewonnen");
-    } else if (
-      my_button_1.value == 2 &&
-      my_button_5.value == 2 &&
-      my_button_9.value == 2
-    ) {
-      console.log("Spieler 2 hat gewonnen");
-    } else if (
-      my_button_3.value == 2 &&
-      my_button_5.value == 2 &&
-      my_button_7.value == 2
-    ) {
-      console.log("Spieler 2 hat gewonnen");
-    } else if (
-      my_button_1.value == 2 &&
-      my_button_4.value == 2 &&
-      my_button_7.value == 2
-    ) {
-      console.log("Spieler 2 hat gewonnen");
-    } else if (
-      my_button_2.value == 2 &&
-      my_button_5.value == 2 &&
-      my_button_8.value == 2
-    ) {
-      console.log("Spieler 2 hat gewonnen");
-    } else if (
-      my_button_3.value == 2 &&
-      my_button_6.value == 2 &&
-      my_button_9.value == 2
-    ) {
-      console.log("Spieler 2 hat gewonnen");
-    }
-  } else {
+  if (roundCounter === 9) {
     console.log("unentschieden");
+    return;
+  }
+  if (checkIfPlayerWon(1) === true) {
+    console.log(`Spieler 1 hat gewonnen`);
+  } else if (checkIfPlayerWon(2) === true) {
+    console.log(`Spieler 2 hat gewonnen`);
+  }
+}
+
+function checkIfPlayerWon(player) {
+  if (
+    my_button_1.value == player &&
+    my_button_2.value == player &&
+    my_button_3.value == player
+  ) {
+    return true;
+  } else if (
+    my_button_4.value == player &&
+    my_button_5.value == player &&
+    my_button_6.value == player
+  ) {
+    return true;
+  } else if (
+    my_button_7.value == player &&
+    my_button_8.value == player &&
+    my_button_9.value == player
+  ) {
+    return true;
+  } else if (
+    my_button_1.value == player &&
+    my_button_5.value == player &&
+    my_button_9.value == player
+  ) {
+    return true;
+  } else if (
+    my_button_3.value == player &&
+    my_button_5.value == player &&
+    my_button_7.value == player
+  ) {
+    return true;
+  } else if (
+    my_button_1.value == player &&
+    my_button_4.value == player &&
+    my_button_7.value == player
+  ) {
+    return true;
+  } else if (
+    my_button_2.value == player &&
+    my_button_5.value == player &&
+    my_button_8.value == player
+  ) {
+    return true;
+  } else if (
+    my_button_3.value == player &&
+    my_button_6.value == player &&
+    my_button_9.value == player
+  ) {
+    return true;
   }
 }
 
@@ -116,14 +75,14 @@ document.querySelectorAll(".game > button").forEach((button) => {
       this.value = 1;
       player = 2;
       roundCounter = roundCounter + 1;
-      console.log(roundCounter);
+      //console.log(roundCounter);
       winningConditions();
     } else if (player == 2) {
       this.innerHTML = "o";
       this.value = 2;
       player = 1;
       roundCounter = roundCounter + 1;
-      console.log(roundCounter);
+      //console.log(roundCounter);
       winningConditions();
     } else {
       console.log(Error);
